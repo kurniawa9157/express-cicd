b1b3af97e8797a5e4c7b83a194cfee8f281ccce0
kurniawan wawan <kurniawa9157@gmail.com>
2024-07-02 14:25:14 +0700

    add route
    Signed-off-by: kurniawan wawan <kurniawa9157@gmail.com>

---
+++ app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Test the /wawan endpoint', () => {
+    it('should respond with a status code of 200 and text "Hello wawan"', async () => {
+        const res = await request(app).get('/wawan');
+        expect(res.status).toBe(200);
+        expect(res.text).toBe('Hello wawan');
+    });
+})
+
+// app.listen(port, ()=>{
+//     console.log('app running on port '+port)
+// })
